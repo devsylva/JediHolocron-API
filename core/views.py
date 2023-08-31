101c09555269d3724a4bc17625b1f5014c76069f
devsylva <sylvaejike@gmail.com>
2023-08-31 23:53:50 +0100

    bug fix

--- core/views.py
+++ core/views.py
@@ -52,7 +52,7 @@ class FilmDetail(APIView):
         serializer = FilmSerializer(film)
         return Response({
             "film": serializer.data,
-            "comment count": film.comments.all().count(),
+            "comment count": Comment.objects.filter(film=film).count(),
             "comments" : film.comment_set.all()
         }, status=status.HTTP_200_OK)
 
