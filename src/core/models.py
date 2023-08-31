9de7d88578099f92cbe544fc5e3e3bdc5f1096e7
devsylva <sylvaejike@gmail.com>
2023-08-31 09:36:20 +0100

    updated comment count

--- src/core/models.py
+++ src/core/models.py
@@ -5,14 +5,16 @@ import uuid
 
 class Film(models.Model):
     title = models.CharField(max_length=255)
+    comment_count = models.PositiveIntegerField(default=0)
     release_date = models.DateField()
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
-    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name="comments")
     text = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
 
---
+++ src/core/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from .models import Comment, Film
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=Comment)
+def update_film_comment_count(sender, instance, created, **kwargs):
+    if created:
+        try:
+            film = Film.objects.get(id=instance.film.id)
+            film.comment_count += 1
+            film.save()
+        except Exception as e:
+            pass
